69d656b4d1997527e5226e3536804a39634f7f00
Joao Guilherme <jaogui@gmail.com>
2019-08-11 04:34:34 -0300

    Projeto iniciado, mostrando ola mundo

---
+++ Testando/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Testando
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vai se fuder");
+
+            Console.ReadLine();
+        }
+    }
+}
