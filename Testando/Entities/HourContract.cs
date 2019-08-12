d30e32d7ee6ad06f5791cdfbd9fca857848e78f1
Joao Guilherme <jaogui@gmail.com>
2019-08-12 03:59:51 -0300

    Primeiro progama utilizando a logica de enums

---
+++ Testando/Entities/HourContract.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Testando.Entities
+{
+    class HourContract
+    {
+        public DateTime Date { get; set; }
+        public double ValuePerHour { get; set; }
+        public int Hours { get; set; }
+
+        public HourContract()
+        {
+        }
+
+        public HourContract(DateTime date, double valueperhour, int hours)
+        {
+            Date = date;
+            ValuePerHour = valueperhour;
+            Hours = hours;
+        }
+
+        public double TotalValue()
+        {
+            return Hours * ValuePerHour;
+        }
+    }
+}
